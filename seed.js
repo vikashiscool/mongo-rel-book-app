3611f75d01e8198bbe7a40620862ba08caee62fd
vikashiscool <vikash_parekh@yahoo.com>
2015-12-26 14:42:25 -0500

    Create comment functionality in view

--- seed.js
+++ seed.js
@@ -5,36 +5,36 @@ var ourBooks = require("books.json");
 
 // Add books to DB
 
-db.Book.create(ourBooks, function(err, book){
-  if(err){ return console.log(err);}
-  console.log("Added everything");
-  process.exit(0);
-})
+// db.Book.create(ourBooks, function(err, book){
+//   if(err){ return console.log(err);}
+//   console.log("Added everything");
+//   process.exit(0);
+// })
 
 
-db.Book.findOne({title: "Ender's Game"}, function(err, book){
-  if(err){ return console.log(err);}
-  var newComment = {
-    comment: "I could totally guess the ending.",
-    person: "Justin"
-  };
-  book.comments.push(newComment);
-  book.save(function (err, success){
-  if(err) { return console.log(err)}
-  console.log("Comment added successfully");
-  })
-});
+// db.Book.findOne({title: "Ender's Game"}, function(err, book){
+//   if(err){ return console.log(err);}
+//   var newComment = {
+//     comment: "I could totally guess the ending.",
+//     person: "Justin"
+//   };
+//   book.comments.push(newComment);
+//   book.save(function (err, success){
+//   if(err) { return console.log(err)}
+//   console.log("Comment added successfully");
+//   })
+// });
 
 
-//Remove books from DB  
-db.Book.removeBook({title: "Ender's Game"}, function(err, book){
-  if(err) {return console.log(err);}
-  book.remove;
-  book.save(function (err, success){
-    if(err) { return console.log(err);}
-    console.log('Book removed successfully');
-  })
-});
+// //Remove books from DB  
+// db.Book.removeBook({title: "Ender's Game"}, function(err, book){
+//   if(err) {return console.log(err);}
+//   book.remove;
+//   book.save(function (err, success){
+//     if(err) { return console.log(err);}
+//     console.log('Book removed successfully');
+//   })
+// });
 
 
 
